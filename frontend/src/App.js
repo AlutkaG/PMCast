bba09bba232f0a2a51b200ed9182257400f1759a
AlutkaG <50596853+AlutkaG@users.noreply.github.com>
2021-03-25 17:21:55 +0100

    Created detonaror

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,13 @@ function App() {
       <WelcomeSection />
       <Infobar />
       <About />
-      <TimeLine />
+      <TimeLine
+        year={2021}
+        month={[4, 4, 4, 4, 5, 5, 5, 5]}
+        day={[6, 13, 20, 27, 4, 11, 18, 25]}
+        hour={19}
+        minutes={0}
+      />
     </div>
   );
 }
